26bbf8e5cadf6cf48257c612e3483fce2c8a0fea
mohityede <mohityede755@gmail.com>
2024-02-08 21:17:32 +0530

    header ui updated

--- mykart-client/src/components/header.tsx
+++ mykart-client/src/components/header.tsx
@@ -1,31 +1,42 @@
 import { Link } from "react-router-dom"
 import logo from "../assets/MyKart-logo.png"
+import { MdHome, MdSearch } from "react-icons/md"
+import { CgProfile } from "react-icons/cg"
+import { PiSignOut } from "react-icons/pi"
+import { IoMdCart } from "react-icons/io"
+import { useState } from "react"
 
 const user = { _id: 123 }
 
 function Header() {
+  const [isDialog, setisDialog] = useState<boolean>(false);
+
   return (
     <div className="header">
-
       <img className="logo-img" src={logo} alt="mykart-logo" />
-
       <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/search"}>Search</Link>
-        <Link to={"/cart"}>cart</Link>
+        <Link to={"/"}>
+          <MdHome />
+        </Link>
+        <Link to={"/search"}>
+          <MdSearch />
+        </Link>
+        <Link to={"/cart"}>
+          <IoMdCart />
+        </Link>
         {
           user?._id ? (
             <>
-              <button>
-                profile
-              </button>
-              <dialog>
+
+              <CgProfile onClick={() => setisDialog((prev) => !prev)} />
+
+              <dialog open={isDialog}>
                 <div>
                   <Link to="/orders">
                     Orders
                   </Link>
                   <button >
-                    signOut
+                    signOut <PiSignOut />
                   </button>
                 </div>
               </dialog>
