36c0ff4c7025f0d12bee6d3d8778185e18a121cb
mohityede <mohityede755@gmail.com>
2024-03-14 21:20:13 +0530

    home page UI created

---
+++ mykart-client/src/components/card.tsx
@@ -0,0 +1,24 @@
+type ProductProps = {
+  productId: string;
+  name: string;
+  photo: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+
+const server = "dajfjoj";
+function Card({ productId, name, photo, price, stock, handler }: ProductProps) {
+  return (
+    <div className="productcard">
+      <img src={`${photo}`} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+      <div>
+        <button onClick={() => handler()}>add to cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- mykart-client/src/pages/home.tsx
+++ mykart-client/src/pages/home.tsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+import Card from "../components/card";
+
 function Home() {
+  const addToCartHandler = () => {};
   return (
-    <>
-      Home
-    </>
-  )
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          more...
+        </Link>
+      </h1>
+      <main>
+        <Card
+          productId="9784"
+          name="camera"
+          photo="https://m.media-amazon.com/images/I/713xBPyXC-L._SL1500_.jpg"
+          price={420}
+          stock={1000}
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
